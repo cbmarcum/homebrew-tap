2f6c6b1ec113911ac8ddedab14332eb971254f95
jreleaserbot <jreleaser@kordamp.org>
2022-09-18 17:27:46 -0400

    app v1.0.1

--- Formula/app.rb
+++ Formula/app.rb
@@ -1,10 +1,10 @@
-# Generated with JReleaser 1.2.0 at 2022-09-18T16:58:59.622303-04:00
+# Generated with JReleaser 1.2.0 at 2022-09-18T17:27:37.90377-04:00
 class App < Formula
   desc "Sample application"
   homepage "https://acme.com/awesome-app"
-  url "https://github.com/cbmarcum/jreleaser-example-app/releases/download/v1.0.0/app-1.0.0.zip"
-  version "1.0.0"
-  sha256 "6ae7ae152e44800a91cc06ac6a5424062348b7d99c27ac3e451a41a3c1d3f9bd"
+  url "https://github.com/cbmarcum/jreleaser-example-app/releases/download/v1.0.1/app-1.0.1.zip"
+  version "1.0.1"
+  sha256 "23d846c6b7c04a7d99530fec72e696b2a856d39c2e1d235183822f01c61fc62f"
   license "Apache-2.0"
 
   depends_on "openjdk@8"
@@ -16,6 +16,6 @@ class App < Formula
 
   test do
     output = shell_output("#{bin}/app --version")
-    assert_match "1.0.0", output
+    assert_match "1.0.1", output
   end
 end
